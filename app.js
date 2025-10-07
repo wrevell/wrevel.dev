0a8fa80ccf9e01951ed58a35b9b3995faa2761cc
wrevell <90734471+wrevell@users.noreply.github.com>
2025-10-06 20:28:27 -0600

    added background elements and changed some text
    pretty big appearance change with some new effects and a little javascript, hooray!

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-// mostly, if not all, used from Train To Code's tutorial on this topic. see his video here -> https://www.youtube.com/watch?v=nDPsLFPzVEA
-let observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("inview");
-      observer.unobserve(entry.target);
-    }
-  });
-});
+window.onload = () => {
+  // mostly, if not all, used from Train To Code's tutorial on this topic. see his video here -> https://www.youtube.com/watch?v=nDPsLFPzVEA
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("inview");
+        observer.unobserve(entry.target);
 
-document.querySelectorAll('#entercontroller').forEach(el => {
-  observer.observe(el);
-});
+
+      }
+    });
+  });
+  document.querySelectorAll('#entercontroller').forEach(el => {
+    observer.observe(el);
+  });
+}
